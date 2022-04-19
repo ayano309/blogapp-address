12abde5b198edea8cb79ecfe015df568c671ed45
ayano309 <ayatomato.k@gmail.com>
2022-04-19 14:54:20 +0000

    [finish]課題

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'homes/index'
-  get '/about' => "homes#about"
+  root to: 'homes#index'
+   get "home/about" => "homes#about", as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#index"
   resources :books
-  resources :users
+   resources :users ,only: [:index, :show, :edit, :update]
 end
