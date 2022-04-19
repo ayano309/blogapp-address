91b244aa7b6830b63d0cedd12b2dee39fb694d19
ayano309 <ayatomato.k@gmail.com>
2022-04-19 06:51:33 +0000

    名前でログインできるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'homes/index'
-  get 'homes/about'
+  get '/about' => "homes#about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "homes#index"
   resources :books
   resources :users
 end
