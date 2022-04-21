0b616c8ff2848fc95656305e25ecad91a8f1fa4f
ayano309 <ayatomato.k@gmail.com>
2022-04-21 05:53:54 +0000

    検索機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,20 @@ class User < ApplicationRecord
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+  
   def follow!(user)
     user_id = get_user_id(user)
      
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'followings/index'
-  get 'followers/index'
   root to: 'homes#index'
-   get "home/about" => "homes#about", as: "about"
+  get "home/about" => "homes#about", as: "about"
+  get "search" => "searches#search"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books do
